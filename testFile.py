874a91282c7f2012827d2508d9698b34a08bfb5c
varnittewari <vxt6823@rit.edu>
2019-12-16 22:44:25 -0500

    uploaded the jupyter notebook

--- testFile.py
+++ testFile.py
@@ -1,9 +1,13 @@
 import pandas as pd
+import numpy as np
+import itertools
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from matplotlib.pylab import rcParams
+from tqdm import tqdm_notebook as tqdm
+import _pickle as pickle
+plt.style.use('ggplot')
 
-bc = pd.read_csv('BTC-USD.csv')
-
-bc['Date'] = pd.to_datetime(bc.Date)
-
-bc.set_index('Date', inplace= True)
-
-bc = bc[['Close']].loc['2017-01-01':]
