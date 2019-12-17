d2132b3cc2f4f87ec2d8f489e5c5a7d156ee812d
varnittewari <vxt6823@rit.edu>
2019-12-16 20:38:08 -0500

    initial work

--- testFile.py
+++ testFile.py
@@ -1,4 +1,9 @@
-def main():
-    print("Hello World")
+import pandas as pd
 
-main()
+bc = pd.read_csv('BTC-USD.csv')
+
+bc['Date'] = pd.to_datetime(bc.Date)
+
+bc.set_index('Date', inplace= True)
+
+bc = bc[['Close']].loc['2017-01-01':]
